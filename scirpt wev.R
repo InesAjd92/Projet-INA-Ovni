f1462979a877d8153f0ff1eaf9d1e305d67df970
Ines A. <ines.ajd@gmail.com>
2024-03-05 23:18:19 +0100

    Ajout des fichiers utilisés les deux premiers jours du projet

---
+++ scirpt wev.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+
+library(readr)
+donnees_web <- read_csv("~/MIP Ovni/data/noticesvideoNonmedia_30video.csv")
+
+test <-  read_csv("~/MIP Ovni/data/noticesvideoNonmedia_30video.csv")
+
+
+
+
+
---
+++ script pour  mip ovni.R
@@ -0,0 +1,35 @@
+library(tidyverse)
+
+df <- corpus11_ovni
+mots_cles <- MotsClesCorpusOvni
+mots_cles_pertinent <- c("extraterrestre", "ovni")
+
+
+# MIP Ovni
+
+# Corpus OVNI
+
+# Informations sur les colonnes
+
+
+# Contexte de l'analyse ---------------------------------------------------
+
+# Dans le cadre d'un objet de travail sur les archives numériques de l'INA, nous avons choisi de parler du sujet des OVNI et extraterrestres. 
+# Nous souhaitons savoir si il existe une différence de représentation selon les médias de communication. 
+
+# Pour cela, nous avons établi des questions de recherche  
+
+# -1/
+# -2/ ...
+
+
+
+# Appropriation et nettoyage des données ----------------------------------
+
+# a) Chargement des librairies
+
+library(tidyverse)
+library(skimr)
+library(rstatix)
+library(questionr)
+library(readr)
